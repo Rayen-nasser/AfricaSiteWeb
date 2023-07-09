e1b0c38eb3d8e6445aa21e206f046c591a2ced09
Rayen-nasser <111684397+Rayen-nasser@users.noreply.github.com>
2023-07-09 10:52:34 +0100

    change file js and css

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 "missing some adding.." 
+=======
+<<<<<<< HEAD
+# AfricaSiteWeb
+=======
+"missing some adding.."
+"show the section through which the user changes (add code in js)" 
+>>>>>>> ea31d66 (Loading...)
+>>>>>>> Stashed changes
--- js/script.js
+++ js/script.js
@@ -8,4 +8,26 @@ var typed = new Typed(".typing" ,{
 
 function toggle(){
     sec.classList.toggle('active')
+<<<<<<< Updated upstream
 };
+=======
+};
+
+
+// Get all the <li> elements inside the <ul> with class "nav"
+const navItems = document.querySelectorAll('.nav li a');
+
+// Iterate over each <li> element
+navItems.forEach((item) => {
+  // Add a click event listener to each <li> element
+  item.addEventListener('click', () => {
+    // Remove the "active" class from all <li> elements
+    navItems.forEach((navItem) => {
+      navItem.classList.remove('active');
+    });
+    
+    // Add the "active" class to the clicked <li> element
+    item.classList.add('active');
+  });
+});
+>>>>>>> Stashed changes
