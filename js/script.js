ea31d66198ea4f0e2fafac6a5b5bab75633f8665
Rayen-nasser <111684397+Rayen-nasser@users.noreply.github.com>
2023-07-09 09:28:07 +0100

    Loading...

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*****  typing animation  *****/
+var typed = new Typed(".typing" ,{
+    strings:["","Web Designer","","Web Developer",""],
+    typeSpeed:100,
+    BackSpeed:90,
+    loop:true,
+});
+
+function toggle(){
+    sec.classList.toggle('active')
+};
